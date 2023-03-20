91f6ef54723a824066563665d003135e9c68bdf9
Konstantin BIFERT <kissu@baserow.io>
2023-03-20 11:33:49 +0100

    feature: initial vueuse fetch example

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    Change the ID: <input type="text" v-model="id" />
+    <button @click="execute">execute</button>
+
+    <br />
+    <div v-if="error" style="color: red">Error: {{ error }}</div>
+    <div v-else-if="isFetching" style="color: blue">loading...</div>
+    <pre v-else>{{ data }}</pre>
   </main>
 </template>
+
+<script setup>
+import { ref, computed } from 'vue'
+import { useFetch } from '@vueuse/core'
+const id = ref(1)
+const url = computed(() => `https://jsonplaceholder.typicode.com/todos/${id.value}`)
+const { execute, isFetching, data, error } = useFetch(url, { immediate: false })
+</script>
